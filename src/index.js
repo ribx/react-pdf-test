42ed4179f1f66f7dfa352abfba589084717748b6
Robin Lutz <robin.m.lutz@gmail.com>
2019-03-16 12:03:36 +0100

    react-pdf test with redux and react-intl provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react'
+import {createStore} from 'redux'
+import ReactDOM from 'react-dom'
+import {IntlProvider, FormattedMessage} from 'react-intl'
+import {Provider as ReduxProvider, connect} from 'react-redux'
+import {Document, Page, View, Text, PDFViewer} from '@react-pdf/renderer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(state => state)
+
+const Connected = connect(state => ({state}))(props => console.log('state', props.state) || props.children)
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PDFViewer>
+          <Document>
+            <Page>
+              <View>
+                <Text>
+                  <FormattedMessage id="test">{s => s}</FormattedMessage>
+                </Text>
+              </View>
+              <View>
+                <Text>
+                  <Connected>
+                    Redux connected component Test
+                  </Connected>
+                </Text>
+              </View>
+            </Page>
+          </Document>
+        </PDFViewer>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <ReduxProvider store={store}>
+    <IntlProvider locale="en" messages={{en: {id: "test", defaultMessage: "React PDF Test"}}}>
+      <App/>
+    </IntlProvider>
+  </ReduxProvider>,
+  document.getElementById('root'),
+)
